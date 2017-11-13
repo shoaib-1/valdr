ccda7e934357d2ea28549b5ea587eeb328ef7fbb
shoaib-1 <shoaib@nineleaps.com>
2017-11-13 20:19:53 +0530

    Removed other files

--- CONTRIBUTING.md
+++
@@ -1,6 +0,0 @@
-# Contributing Guide
-
-
-## Commit messages
-
-Follow the [AngularJS conventions](https://docs.google.com/document/d/1QrDFcIiPjSLDn3EL15IJygNPiHORgU1_OOAqWjiDU5Y/edit).
--- valdr-message.js
+++ valdr-message.js
@@ -1,3 +1,106 @@
+(function (window, document) {
+'use strict';
+
+angular.module('valdr')
+
+/**
+ * This service provides shared configuration between all valdr-message directive instances like the configured
+ * template to render the violation messages and whether or not angular-translate is available.
+ */
+  .provider('valdrMessage', function () {
+
+    var userDefinedTemplateUrl, userDefinedTemplate,
+      messages = {},
+      defaultTemplateUrl = 'valdr/default-message.html',
+      defaultTemplate =   '<div class="valdr-message">' +
+                            '{{ violation.message }}' +
+                          '</div>',
+      translateTemplate = '<div class="valdr-message" ng-show="violation">' +
+                            '<span ' +
+                            'translate="{{ violation.message }}" ' +
+                            'translate-values="violation"></span>' +
+                          '</div>';
+
+    this.setTemplate = function (template) {
+      userDefinedTemplate = template;
+    };
+
+    this.setTemplateUrl = function (templateUrl) {
+      userDefinedTemplateUrl = templateUrl;
+    };
+
+    this.addMessages = function (newMessages) {
+      angular.extend(messages, newMessages);
+    };
+    var addMessages = this.addMessages;
+
+    this.getMessage = function (typeName, fieldName, validatorName) {
+      var fullMessageKey = typeName + '.' + fieldName + '.' + validatorName;
+      return messages[fullMessageKey] || messages[validatorName] || '[' + validatorName + ']';
+    };
+    var getMessage = this.getMessage;
+
+    this.$get = ['$templateCache', '$injector', function ($templateCache, $injector) {
+
+      var angularMessagesEnabled = false;
+
+      function getTranslateService() {
+        try {
+          return $injector.get('$translate');
+        } catch (error) {
+          return undefined;
+        }
+      }
+
+      function getFieldNameKeyGenerator() {
+        try {
+          return $injector.get('valdrFieldNameKeyGenerator');
+        } catch (error) {
+          return function(violation) {
+            return violation.type + '.' + violation.field;
+          };
+        }
+      }
+
+      var $translate = getTranslateService(),
+        translateAvailable = angular.isDefined($translate),
+        fieldNameKeyGenerator = getFieldNameKeyGenerator();
+
+      function determineTemplate() {
+        if (angular.isDefined(userDefinedTemplate)) {
+          return userDefinedTemplate;
+        } else if (translateAvailable) {
+          return translateTemplate;
+        } else {
+          return defaultTemplate;
+        }
+      }
+
+      function updateTemplateCache() {
+        $templateCache.put(defaultTemplateUrl, determineTemplate());
+        if (userDefinedTemplateUrl && userDefinedTemplate) {
+          $templateCache.put(userDefinedTemplateUrl, userDefinedTemplate);
+        }
+      }
+
+      updateTemplateCache();
+
+      return {
+        templateUrl: userDefinedTemplateUrl || defaultTemplateUrl,
+        setTemplate: function (newTemplate) {
+          userDefinedTemplate = newTemplate;
+          updateTemplateCache();
+        },
+        translateAvailable: translateAvailable,
+        $translate: $translate,
+        fieldNameKeyGenerator: fieldNameKeyGenerator,
+        addMessages: addMessages,
+        getMessage: getMessage,
+        angularMessagesEnabled: angularMessagesEnabled
+      };
+    }];
+  });
+
 /**
  * This directive appends a validation message to the parent element of any input, select or textarea element, which
  * is nested in a valdr-type directive and has an ng-model bound to it.
@@ -82,7 +185,8 @@ angular.module('valdr')
             angular.forEach(scope.violations, function (violation) {
               valdrMessage.$translate(valdrMessage.fieldNameKeyGenerator(violation)).then(function (translation) {
                 violation.fieldName = translation;
-              });
+              })
+              .catch(angular.noop);
             });
           }
         };
@@ -151,3 +255,5 @@ angular.module('valdr')
       }
     };
   }]);
+
+})(window, document);
